83a6c379da69eaace4048228612395e7559880c0
mojnomiya <mearn4706@gmail.com>
2023-08-06 15:02:44 +0600

    after adding style

---
+++ src/pages/NotePage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import notes from "../assets/data";
+import { Link } from "react-router-dom";
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
+
+const NotePage = () => {
+  const { id } = useParams();
+
+  let note = notes.find((note) => note.id === Number(id));
+  return (
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <Link to="/">
+            <ArrowLeft />
+          </Link>
+        </h3>
+      </div>
+      <textarea value={note?.body}></textarea>
+    </div>
+  );
+};
+
+export default NotePage;
